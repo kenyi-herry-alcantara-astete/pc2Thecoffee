749bf7fde867aaf0ec089d4f04b0b7b98c529b6e
Jose Caycho <jose.caycho.v@uni.pe>
2022-06-05 03:47:36 -0500

    Añadiendo Pregunta 7

--- src/main/java/Fase4/Produccion/PremiumFlight.java
+++ src/main/java/Fase4/Produccion/PremiumFlight.java
@@ -11,14 +11,19 @@ public class PremiumFlight extends Flight {
 
     @Override
     public boolean addPassenger(Passenger passenger) {
+        if (passenger.isVip()) {
+            return passengers.add(passenger);
+        }
         return false;
     }
 
     @Override
     public boolean removePassenger(Passenger passenger) {
+        if (passenger.isVip()) {
+            return passengers.remove(passenger);
+        }
         return false;
     }
-
 
 }
 
